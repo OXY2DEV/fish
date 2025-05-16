f2514b972b199ed9708060c4d9a3e35934a65524
Shawon <mdmoinulhossainshawon@gmail.com>
2025-05-16 18:04:25 +0600

    feat(prompt): Added last command status

---
+++ functions/get_status.fish
@@ -0,0 +1,11 @@
+function get_status -d "Creates status badge for given status."
+	set_color normal;
+
+	if test $argv[1] -eq 0;
+		set_color 94E2D5;
+		echo " 󰁨 $argv[1] ";
+	else
+		set_color F38BA8;
+		echo " 󰅙 $argv[1] ";
+	end
+end
