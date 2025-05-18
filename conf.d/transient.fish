b3bf60bd19a784a1e4f6ce4c01ca665a94d40e06
Shawon <mdmoinulhossainshawon@gmail.com>
2025-05-18 08:45:48 +0600

    refactor(transience): Changed transient prompt detection logic

--- conf.d/transient.fish
+++ conf.d/transient.fish
@@ -1,46 +1,14 @@
 function update_prompt
-	commandline -f expand-abbr suppress-autosuggestion
-
-	if commandline --is-valid || test -z "$(commandline)"
-		if commandline --paging-mode && test -n "$(commandline)"
-			commandline -f accept-autosuggestion;
-			return;
-		end
-
+	# Activate transient prompt if,
+	#     1. Command-line is valid.
+	#     2. Command-line is empty & not in paging mode.
+	if commandline --is-valid || test -z $(commandline | string collect) && not commandline --paging-mode
 		set -g transient 1;
-		commandline -f repaint execute;
-		return;
+		commandline -f repaint;
 	end
 
-	commandline -f execute
+	commandline -f execute;
 end
-
-# function set_state
-# 	commandline --function expand-abbr suppress-autosuggestion
-#
-# 	if commandline --is-valid || test -z "$(commandline)"↩
-# 		if commandline --paging-mode && test -n "$(commandline)"
-# 			commandline -f accept-autosuggestion;
-# 			return 0;
-# 		end
-#
-# 		set -g TRANSIENT 1;
-# 		commandline --function repaint execute
-# 		return 0;
-# 	end
-#
-# 	commandline -f execute
-# end
 
 bind --user --mode default \r update_prompt
 bind --user --mode insert \r update_prompt
-
-
-# bind --user --mode default \cj set_state
-# bind --user --mode insert \cj set_state
-#
-# bind --user --mode default \cc set_state
-# bind --user --mode insert \cc set_state
-#
-# bind --user --mode default \cd set_state
-# bind --user --mode insert \cd set_state
